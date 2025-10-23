c3bea640683aa93ce92e4ff7e4bcedc0007cf286
mhndrd555-star <mhnd.rd555@gmail.com>
2025-10-23 22:56:44 +0300

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Discord Bot Token
+    DISCORD_TOKEN: 'your_discord_bot_token_here',
+    
+    // Discord Application ID
+    CLIENT_ID: 'your_discord_application_id_here'
+};
